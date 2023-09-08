588ffdc6b9a28791c6c8f5df0ff70633551f0466
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-09-08 11:12:23 +0700

    pusing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -137,11 +137,15 @@
         <!-- Main Footer -->
         @include('layouts.footer')
 
+        <!-- Korang tengok Ni -->
+        @stack ('script')
+
         <!-- ./wrapper -->
 
         <!-- REQUIRED SCRIPTS -->
         <!-- jQuery -->
         <script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.8/jquery.inputmask.min.js"></script>
         <!-- Bootstrap 4 -->
         <script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <!-- AdminLTE App -->
